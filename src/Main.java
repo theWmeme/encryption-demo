831d5c6157f5ba015d1065698d2f436ad8bf4d51
126EWright <126ewright@gmail.com>
2025-12-16 12:51:36 -0500

    start2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
+
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        //step 1: define what you are trying to access (word, text, whatever)
+        //step 2: set a password
+        //step 3: form an algorithm that will scramble whatever your password is (set as "encrypted version")
+        //step 4: give a guide for descrambling the password
+        //step 5: make user guess based on scrambler
+        System.out.println("Welcome to my simple encryption demo! \nWould you like to try and decrypt the text yourself, or have it decrypted for you:");
+        System.out.println("1 = decrypt myself \n2 = decrypt for me");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        if(choice == 1) {
+            System.out.println("You have decided to decrypt it yourself yourself!");
+        }
+        else if(choice == 2) {
+            System.out.println("You want me to decrypt the chosen text");
+        }
+
 
     }
 }
